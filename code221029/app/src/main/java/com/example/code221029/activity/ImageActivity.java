6b659c5f2cc930479c00af0df57cd1474c5fb69d
uda97 <insky731@gmail.com>
2022-11-10 22:37:11 +0900

    File: activity_image.xml Function: 문자추출 Reference: https://codelabs.developers.google.com/codelabs/mlkit-android#2

--- code221029/app/src/main/java/com/example/code221029/activity/ImageActivity.java
+++ code221029/app/src/main/java/com/example/code221029/activity/ImageActivity.java
@@ -1,18 +1,37 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.example.code221029.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.code221029.R;
@@ -24,56 +43,90 @@ import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
-public class ImageActivity extends AppCompatActivity {
-
-    private static final String TAG="ImageActivity";
+public class ImageActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static final String TAG = "ImageAcrivity";
     private ImageView mImageView;
     private Button mTextButton;
     private Button mCameraButton;
+    private TextView mTextView;
     private Bitmap mSelectedImage;
     private GraphicOverlay mGraphicOverlay;
+    // Max width (portrait mode)
     private Integer mImageMaxWidth;
+    // Max height (portrait mode)
     private Integer mImageMaxHeight;
 
-    //Number of results to show in the UI.
-    private static final int RESULTS_TO_SHOW=3;
-
-    //Dimensions of inputs.
+    /**
+     * Number of results to show in the UI.
+     */
+    private static final int RESULTS_TO_SHOW = 3;
+    /**
+     * Dimensions of inputs.
+     */
     private static final int DIM_IMG_SIZE_X = 224;
     private static final int DIM_IMG_SIZE_Y = 224;
+
+    private final PriorityQueue<Map.Entry<String, Float>> sortedLabels =
+            new PriorityQueue<>(
+                    RESULTS_TO_SHOW,
+                    new Comparator<Map.Entry<String, Float>>() {
+                        @Override
+                        public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float>
+                                o2) {
+                            return (o1.getValue()).compareTo(o2.getValue());
+                        }
+                    });
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.example.code221029.R.layout.activity_image);
+        setContentView(R.layout.activity_image);
 
-        mImageView = findViewById(R.id.image_view);
+        mImageView = (ImageView) findViewById(R.id.image_view);
 
-        mTextButton = findViewById(R.id.button_text);
+        mTextButton = (Button)findViewById(R.id.button_text);
+        mCameraButton =(Button) findViewById(R.id.button_camera);
+        mTextView=(TextView) findViewById(R.id.text_view_result);
 
-        mGraphicOverlay = findViewById(R.id.graphic_overlay);
+        mGraphicOverlay =(GraphicOverlay) findViewById(R.id.graphic_overlay);
+
         mTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 runTextRecognition();
             }
         });
-
-        mSelectedImage = getBitmapFromAsset(this, "Please_walk_on_the_grass.jpg");
-
-
-
+        mCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ImageActivity.this,CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+        Spinner dropdown = findViewById(R.id.spinner);
+        String[] items = new String[]{"Test Image 1 (English)", "Test Image 2 (Korean)","camera temp image"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout
+                .simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);
     }
+
 
 
     private void runTextRecognition() {
         InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
-        TextRecognizer recognizer = TextRecognition.getClient();
+        TextRecognizer recognizer = TextRecognition.getClient(new KoreanTextRecognizerOptions.Builder().build());
         mTextButton.setEnabled(false);
         recognizer.process(image)
                 .addOnSuccessListener(
@@ -82,6 +135,7 @@ public class ImageActivity extends AppCompatActivity {
                             public void onSuccess(Text texts) {
                                 mTextButton.setEnabled(true);
                                 processTextRecognitionResult(texts);
+                                mTextView.setText(texts.getText());
                             }
                         })
                 .addOnFailureListener(
@@ -114,6 +168,8 @@ public class ImageActivity extends AppCompatActivity {
             }
         }
     }
+
+
 
     private void showToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
@@ -161,17 +217,18 @@ public class ImageActivity extends AppCompatActivity {
         return new Pair<>(targetWidth, targetHeight);
     }
 
-
-
+    @Override
     public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
         mGraphicOverlay.clear();
         switch (position) {
             case 0:
-                mSelectedImage = getBitmapFromAsset(this, "Please_walk_on_the_grass.jpg");
+                mSelectedImage = getBitmapFromAsset(this, "english_text_sample.jpg");
                 break;
             case 1:
-                // Whatever you want to happen when the thrid item gets selected
-                mSelectedImage = getBitmapFromAsset(this, "grace_hopper.jpg");
+                mSelectedImage = getBitmapFromAsset(this, "korean_text_sample.jpg");
+                break;
+            case 2:
+                mSelectedImage=getBitmapFromAsset(this, "camera_temp_sample.jpg");
                 break;
         }
         if (mSelectedImage != null) {
@@ -199,6 +256,7 @@ public class ImageActivity extends AppCompatActivity {
         }
     }
 
+    @Override
     public void onNothingSelected(AdapterView<?> parent) {
         // Do nothing
     }
